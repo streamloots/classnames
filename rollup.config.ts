7862a6b1859e6c011090adb4aae9fda128f5e14a
gabriel garcia <ggarciaseco@gmail.com>
2020-05-18 18:05:19 +0200

    remove terser

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,5 +1,4 @@
 import typescript from 'rollup-plugin-typescript2';
-import { terser } from 'rollup-plugin-terser';
 
 import pkg from './package.json';
 
@@ -22,7 +21,6 @@ export default {
   plugins: [
     typescript({
       typescript: require('typescript')
-    }),
-    terser()
+    })
   ]
 };
